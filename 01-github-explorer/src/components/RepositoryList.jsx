039659bbe30ffcef23aa0f083abe2853f6eabf68
Bruno Bergamaschi <brunobergamaschi@live.com>
2021-12-08 16:11:42 -0300

    Utilizando o UseEffect + Listando repositórios

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,22 +1,28 @@
 import { RepositoryItem } from './RepositoryItem';
 import '../styles/repositories.scss';
+import { useEffect, useState } from 'react';
 
-const repository = {
-	name: 'unform',
-	description: 'Forms in React',
-	link: 'https://github.com/bruno-bergamaschi/Rocketseat_RectJS',
-};
+//https://api.github.com/users/bruno-bergamaschi/repos
 
 export function RepositoryList() {
+	const [repositories, setRepositories] = useState([]);
+
+	useEffect(() => {
+		fetch('https://api.github.com/users/bruno-bergamaschi/repos')
+			.then((response) => response.json())
+			.then((data) => setRepositories(data));
+	}, []);
+	// cuidado para não deixar sem o segundo parâmetro
+	// entrará em looping
+
 	return (
 		<section className='repository-list'>
 			<h1>Lista de repositórios</h1>
 
 			<ul>
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
-				<RepositoryItem repository={repository} />
+				{repositories.map((rep) => {
+					return <RepositoryItem key={rep.name} repository={rep} />;
+				})}
 			</ul>
 		</section>
 	);
